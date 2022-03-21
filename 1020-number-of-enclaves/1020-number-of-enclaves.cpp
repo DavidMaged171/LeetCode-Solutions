0d5e5acae602bb0451c799a672d6411ae24162ec
DavidMaged171 <48919051+DavidMaged171@users.noreply.github.com>
2022-03-21 12:37:14 +0200

    Time: 398 ms (5.04%), Space: 80.2 MB (5.56%) - LeetHub

---
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -0,0 +1,65 @@
+class Solution {
+public:
+    int numEnclaves(vector<vector<int>>& grid) {
+        int rows=grid.size(),cols=grid[0].size();
+        int sum=0,sumAll=0;
+        vector<vector<int>>moves={{1,0},{-1,0},{0,1},{0,-1}};
+        queue<vector<int>>q;
+        
+        for(int i=0;i<rows;i++)
+        {
+            for(int g=0;g<cols;g++)
+            {
+                sum=0;
+                bool isClosed=(grid[i][g]==1);
+                if(grid[i][g]==1)
+                {
+                    sum++;
+                    q.push({i,g});
+                    if(isCellOnBorder(i,g,rows,cols))
+                        isClosed=false;
+                    
+                }
+                grid[i][g]=2;
+                while(!q.empty())
+                {
+                    auto curr=q.front();
+                    q.pop();
+                    for(auto move:moves)
+                    {
+                        int newX=move[0]+curr[0];
+                        int newY=move[1]+curr[1];
+                        if(isCellValid (newX,newY,rows,cols))
+                        {
+                            if(grid[newX][newY]==1)
+                            {
+                                q.push({newX,newY});
+                                sum++;
+                                if(isCellOnBorder(newX,newY,rows,cols))
+                                    isClosed=false;
+                            }
+                            grid[newX][newY]=2;
+                        }
+                    }
+                }
+                if(isClosed)
+                    sumAll+=sum;
+            }
+        }
+        return sumAll;
+    }
+    
+    bool isCellValid(int x,int y,int rows,int cols)
+    {
+        if(x<0||y<0||x>=rows||y>=cols)
+            return false;
+        return true;
+    }
+    bool isCellOnBorder(int x,int y,int rows,int cols)
+    {
+        if(x==0||y==0||x==rows-1||y==cols-1)
+            return true;
+        //cout<<x<<" "<<y<<" "<<rows<<" "<<cols<<endl;
+        return false;
+    }
+};
