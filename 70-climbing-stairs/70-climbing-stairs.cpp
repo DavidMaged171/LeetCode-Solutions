ee2b1c5f24764dd90a2a31dae5adceb1c2053730
DavidMaged171 <48919051+DavidMaged171@users.noreply.github.com>
2022-03-24 14:42:17 +0200

    Time: 0 ms (100.00%), Space: 6 MB (55.14%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n==1)return 1;
+        else if(n==2)return 2;
+        int arr[n];
+        arr[0]=1;arr[1]=2;
+        for(int i=2;i<n;i++)
+            arr[i]=arr[i-1]+arr[i-2];
+        return arr[n-1];
+    }
+};
