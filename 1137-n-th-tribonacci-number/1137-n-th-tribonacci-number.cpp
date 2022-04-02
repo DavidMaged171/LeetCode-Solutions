9d89b8fd288d20864ec7b9e0b6c07b75e2f81dd4
DavidMaged171 <48919051+DavidMaged171@users.noreply.github.com>
2022-04-03 01:06:57 +0200

    Time: 0 ms (100.00%), Space: 6 MB (51.63%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+        int arr[n+10];
+        arr[0]=0;
+        arr[1]=1;
+        arr[2]=1;
+        for(int i=3;i<=n;i++)
+        {
+            arr[i]=arr[i-1]+arr[i-2]+arr[i-3];
+        }
+        return arr[n];
+    }
+};
