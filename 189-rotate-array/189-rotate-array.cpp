97e7eeebe482d3d3616ff91c0cd8ef4ecc6536e0
DavidMaged171 <48919051+DavidMaged171@users.noreply.github.com>
2022-04-10 23:29:52 +0200

    Time: 16 ms (99.90%), Space: 25 MB (52.72%) - LeetHub

---
+++ 189-rotate-array/189-rotate-array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+       k=k%nums.size();
+        if(k<0){
+            k+=nums.size();
+        }
+        reverse(nums.end()-k,nums.end());
+        reverse(nums.begin(),nums.end()-k);
+        reverse(nums.begin(),nums.end());
+    }
+};
