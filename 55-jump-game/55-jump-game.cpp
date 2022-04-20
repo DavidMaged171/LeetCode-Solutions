b3e98b1b58a21c32695c2be99365bc38bbf2a254
DavidMaged171 <48919051+DavidMaged171@users.noreply.github.com>
2022-04-20 23:37:28 +0200

    Time: 103 ms (41.31%), Space: 48.5 MB (24.20%) - LeetHub

---
+++ 55-jump-game/55-jump-game.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int maxNow=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(i<=maxNow)
+                maxNow=max(maxNow,i+nums[i]);
+            else
+                break;
+        }
+        if(maxNow>=nums.size()-1) return true;
+            
+        return false;
+    }
+};
