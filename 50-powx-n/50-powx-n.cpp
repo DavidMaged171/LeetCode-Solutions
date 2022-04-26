dbc556e55d2265348a6ee2c669574b580dc65397
DavidMaged171 <48919051+DavidMaged171@users.noreply.github.com>
2022-04-26 19:28:25 +0200

    Time: 3 ms (37.88%), Space: 5.8 MB (96.48%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -1,6 +1,27 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        return pow(x,n);
+        if(x==1)return x;
+        
+        double ans=1;
+        long long nn=n;
+        if(nn<0)nn*=-1;
+        while(nn)
+        {
+           if(nn%2==0)
+           {
+               x*=x;
+               nn/=2;
+           }
+            else 
+            {
+                ans*=x;
+                nn--;
+            }
+            
+        }
+        if(n<0)ans=1/ans;
+        return ans;
     }
+    
 };
