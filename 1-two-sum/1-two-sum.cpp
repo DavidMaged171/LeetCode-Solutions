b2ed071c24f298be05180d541abba926a8f7893d
DavidMaged171 <48919051+DavidMaged171@users.noreply.github.com>
2022-07-14 11:20:29 +0200

    Time: 18 ms (68.66%), Space: 12.4 MB (5.95%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -2,16 +2,38 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         vector<int>res;
-        for(int i=0;i<nums.size();i++)
+        map<int,pair<int,int>>mp;
+        mp[nums[0]].first=0;
+        for(int i=1;i<nums.size();i++)
         {
-            for(int g=i+1;g<nums.size();g++)
-                if(nums[i]+nums[g]==target)
-                {
-                    res.push_back(i);
-                    res.push_back(g);
-                    return res;
-                }
+            if(mp[nums[i]].first==0)mp[nums[i]].first=i;
+            else mp[nums[i]].second=i;
         }
+        sort(nums.begin(), nums.end());
+        int start=0,end=nums.size()-1;
+        while(start<end)
+        {
+            if(nums[start]+nums[end]==target)
+            {
+                cout<<nums[start]<<" "<<nums[end];
+                if(nums[start]==nums[end])
+                {
+                    res.push_back(mp[nums[start]].first);
+                    res.push_back(mp[nums[end]].second);
+                    break;
+                }
+                else
+                {
+                    res.push_back(mp[nums[start]].first);
+                    res.push_back(mp[nums[end]].first);
+                    break;
+                }
+            }
+            else if(nums[start]+nums[end]>target)
+                end--;
+            else start++;
+        }
+        
         return  res;
     }
 };
